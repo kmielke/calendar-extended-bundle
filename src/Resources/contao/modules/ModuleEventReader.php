92f9e83d912e5ce30b8a3f55a279feda558226da
Kester Mielke <mail@kester-mielke.de>
2018-11-06 17:31:41 +0100

    small fix when ignoring endtime...

--- src/Resources/contao/modules/ModuleEventReader.php
+++ src/Resources/contao/modules/ModuleEventReader.php
@@ -220,7 +220,9 @@ class ModuleEventReader extends EventsExt
 
         // Fix date if we have to ignore the time
         if ((int)$objEvent->ignoreEndTime === 1) {
-            $strDate = \Date::parse($objPage->datimFormat, $objEvent->startTime) . $GLOBALS['TL_LANG']['MSC']['cal_timeSeparator'] . \Date::parse($objPage->dateFormat, $objEvent->endTime);
+            // $strDate = \Date::parse($objPage->datimFormat, $objEvent->startTime) . $GLOBALS['TL_LANG']['MSC']['cal_timeSeparator'] . \Date::parse($objPage->dateFormat, $objEvent->endTime);
+            // $strTime = null;
+            $strDate = \Date::parse($objPage->dateFormat, $objEvent->startTime);
             $objEvent->endTime = '';
             $objEvent->time = '';
         }
@@ -233,8 +235,8 @@ class ModuleEventReader extends EventsExt
             $arrRange = deserialize($objEvent->repeatEach);
 
             if (is_array($arrRange) && isset($arrRange['unit']) && isset($arrRange['value'])) {
-            $strKey = 'cal_' . $arrRange['unit'];
-            $recurring = sprintf($GLOBALS['TL_LANG']['MSC'][$strKey], $arrRange['value']);
+                $strKey = 'cal_' . $arrRange['unit'];
+                $recurring = sprintf($GLOBALS['TL_LANG']['MSC'][$strKey], $arrRange['value']);
 
                 if ($objEvent->recurrences > 0) {
                     $until = sprintf($GLOBALS['TL_LANG']['MSC']['cal_until'], \Date::parse($objPage->dateFormat, $objEvent->repeatEnd));
